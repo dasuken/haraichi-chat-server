c9a12398828d3e889802ae4b3d44f217f92f46de
github daisukenoguchi <daisukenoguemon@gmail.com>
2020-12-15 13:48:46 +0900

    cors付けてみた

--- src/index.js
+++ src/index.js
@@ -1,33 +1,38 @@
-const { ApolloServer } = require('apollo-server');
-require('dotenv').config()
-const mongoose         = require('mongoose')
-const typeDefs = require('./graphql/typeDefs')
-const resolvers = require('./graphql/resolvers')
 
-const Comment = require('./models/Comment')
-const Theme   = require('./models/Theme')
-const Radio   = require('./models/Radio');
+const { ApolloServer } = require('apollo-server');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+
+const Comment = require('./models/Comment');
+const Theme = require('./models/Theme');
+const Radio = require('./models/Radio');
 
 mongoose
-  .connect(process.env.MONGO_URI,
-    { useUnifiedTopology: true },
-    { useNewUrlParser: true },
-    { useFindAndModify: false })
-  .then(_ => console.log('DB CONNECTED'))
-  .catch(err => console.log(err))
+	.connect(
+		process.env.MONGO_URI,
+		{ useUnifiedTopology: true },
+		{ useNewUrlParser: true },
+		{ useFindAndModify: false }
+	)
+	.then((_) => console.log('DB CONNECTED'))
+	.catch((err) => console.log(err));
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  formatError: error => ({
-    name: error.name,
-    message: error.message.replace("Context creation failed:", "")
-  }),
-  context: async ({ req }) => {
-    return { Comment, Theme, Radio }
-  }
+  cors: true,
+	typeDefs,
+	resolvers,
+	formatError: (error) => ({
+		name: error.name,
+		message: error.message.replace('Context creation failed:', '')
+	}),
+	context: async ({ req }) => {
+		return { Comment, Theme, Radio };
+	}
 });
 
+
 server.listen(process.env.PORT || 4000).then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
+	console.log(`🚀 Server ready at ${url}`);
 });
