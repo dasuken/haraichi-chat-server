163ff17e3801e4334a6a9ee219d7c647cc4f3912
github daisukenoguchi <daisukenoguemon@gmail.com>
2020-12-30 16:05:59 +0900

    thumbnailに処理追加

--- src/models/Radio.js
+++ src/models/Radio.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const { NIL } = require('uuid')
 
 const RadioSchema = new mongoose.Schema({
   times: {
@@ -26,6 +27,7 @@ RadioSchema.virtual('themeImage').get(function() {
   let image
   if (this.themes) {
     image = this.themes[0].thumbnail
+    if (!image) image = "https://static.tbsradio.jp/wp-content/uploads/2020/05/hp_haraichi_0501.jpg"
   } else {
     image = "https://static.tbsradio.jp/wp-content/uploads/2020/05/hp_haraichi_0501.jpg"
   }
